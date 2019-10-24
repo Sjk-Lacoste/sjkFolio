8b75521bc5bc647ef20185e61193f92626bfc76b
Sjk-Lacoste <tmohlatlole@gmail.com>
2019-10-24 11:55:22 +0200

    Update the style and changed the navigation

--- js/app.js
+++ js/app.js
@@ -1,4 +1,13 @@
 // 'use strict';
+/* Set the width of the sidebar to 250px (show it) */
+function openNav() {
+    document.getElementById("mySidepanel").style.width = "250px";
+}
+  
+  /* Set the width of the sidebar to 0 (hide it) */
+function closeNav() {
+    document.getElementById("mySidepanel").style.width = "0";
+}
 
 // My Name
 let myName  = "Tshepo Mohlatlole";
@@ -13,7 +22,7 @@ let time = setInterval(() => {
     myNameOutput.innerHTML += myName[c];
     // console.log(myName[c]);
     c++;
-}, 500);
+}, 50);
 
 // Navigation
 // window.addEventListener('scroll', function (e) {
